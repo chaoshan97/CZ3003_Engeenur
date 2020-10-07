77cfcc75e4dafc0b544e24dba516f62e95e2ac2c
tanbp0009 <tanbp0009@hotmail.com>
2020-10-07 11:53:08 +0800

    Basic ui

---
+++ Assets/Scripts/UIControllerScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControllerScript : MonoBehaviour
+{
+    public GameObject loginCanvas;
+    public GameObject townCanvas;
+    public GameObject levelSelectCanvas;
+    public GameObject shopCanvas;
+    public GameObject battleCanvas;
+    public GameObject shopScrollView;
+    public GameObject inventoryScrollView;
+    public GameObject questionUI;
+    public GameObject battleMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loginButton()
+    { 
+        //TODO check if login succesful
+
+        loginCanvas.SetActive(false);
+        townCanvas.SetActive(true);
+    }
+
+    public void OpenLevelSelectButton()
+    {
+        townCanvas.SetActive(false);
+        levelSelectCanvas.SetActive(true);
+    }
+
+    public void CloseLevelSelectButton()
+    {
+        levelSelectCanvas.SetActive(false);
+        townCanvas.SetActive(true);
+    }
+
+    public void OpenShopButton()
+    {
+        townCanvas.SetActive(false);
+        shopCanvas.SetActive(true);
+    }
+
+    public void CloseShopButton()
+    {
+        inventoryScrollView.SetActive(false);
+        shopScrollView.SetActive(false);
+        shopCanvas.SetActive(false);
+        townCanvas.SetActive(true);
+    }
+
+    public void OpenShopScrollView()
+    {
+        inventoryScrollView.SetActive(false);
+        shopScrollView.SetActive(true);
+    }
+
+    public void OpenInventoryScrollView()
+    {
+        inventoryScrollView.SetActive(true);
+        shopScrollView.SetActive(false);
+    }
+
+    public void OpenQuestionUI()
+    {
+        questionUI.SetActive(true);
+        battleMenu.SetActive(false);
+    }
+
+    public void OpenBattleCanvas()
+    {
+        battleCanvas.SetActive(true);
+        levelSelectCanvas.SetActive(false);
+    }
+}
