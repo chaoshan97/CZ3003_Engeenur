e4d13ebcd56265a659ffba0b99b06060cc1aeeaa
tanbp0009 <tanbp0009@hotmail.com>
2020-10-13 04:30:59 +0800

    Added skeleton battle system Added skeleton monster prefab

---
+++ Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour
+{
+    private int id;
+    private int health;
+    private int damage;
+
+    public MonsterScript(int _id, int _health, int _damage)
+    {
+        id = _id;
+        health = _health;
+        damage = _damage;
+    }
+
+    public int Id { get => id; set => id = value; }
+    public int Health { get => health; set => health = value; }
+    public int Damage { get => damage; set => damage = value; }
+
+    public void init(int _id, int _health, int _damage)
+    {
+        id = _id;
+        health = _health;
+        damage = _damage;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
