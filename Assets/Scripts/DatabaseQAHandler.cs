ad8b7609a58be9cb1326a444d2c0831e8997c307
peiyan-lim <py.peiyan@gmail.com>
2020-10-14 04:57:54 +0800

    Added Sprites and edited question canvas

---
+++ Assets/Scripts/DatabaseQAHandler.cs
@@ -0,0 +1,69 @@
+﻿//using System.Collections.Generic;
+//using FullSerializer;
+//using Proyecto26;
+
+//public static class DatabaseQAHandler
+//{
+//    private static readonly string databaseURL = $"https://engeenur-17baa.firebaseio.com/";
+
+//    private static fsSerializer serializer = new fsSerializer();
+
+//    public delegate void DelQnCallback();
+//    public delegate void PostQnCallback();
+//    public delegate void GetQnCallback(Qn qn);
+//    public delegate void GetQnsCallback(Dictionary<string, Qn> qns);
+
+
+//    /// <summary>
+//    /// Adds a user to the Firebase Database
+//    /// </summary>
+//    /// <param name="qn"> User object that will be uploaded </param>
+//    /// <param name="qnId"> Id of the user that will be uploaded </param>
+//    /// <param name="callback"> What to do after the user is uploaded successfully </param>
+//    public static void PostQn(Qn qn, PostQnCallback callback)
+//    {
+//        RestClient.Post<Qn>($"{databaseURL}qns/.json", qn).Then(response => { callback(); });
+//    }
+
+//    /// <summary>
+//    /// Retrieves a user from the Firebase Database, given their id
+//    /// </summary>
+//    /// <param name="qnId"> Id of the user that we are looking for </param>
+//    /// <param name="callback"> What to do after the user is downloaded successfully </param>
+//    public static void GetQn(string qnId, GetQnCallback callback)
+//    {
+//        RestClient.Get<Qn>($"{databaseURL}qns/{qnId}.json").Then(qn => { callback(qn); });
+//    }
+
+//    /// <summary>
+//    /// Gets all users from the Firebase Database
+//    /// </summary>
+//    /// <param name="callback"> What to do after all users are downloaded successfully </param>
+//    public static void GetQns(GetQnsCallback callback)
+//    {
+//        RestClient.Get($"{databaseURL}qns.json").Then(response =>
+//        {
+//            var responseJson = response.Text;
+
+//            // Using the FullSerializer library: https://github.com/jacobdufault/fullserializer
+//            // to serialize more complex types (a Dictionary, in this case)
+//            var data = fsJsonParser.Parse(responseJson);
+//            object deserialized = null;
+//            serializer.TryDeserialize(data, typeof(Dictionary<string, Qn>), ref deserialized);
+
+//            var qns = deserialized as Dictionary<string, Qn>;
+//            callback(qns);
+//        });
+//    }
+
+//    public static void DeleteQn(string qnKey, DelQnCallback callback)
+//    {
+//        RestClient.Delete($"{databaseURL}qns/" + qnKey + ".json").Then(response => { callback(); });
+//    }
+
+//    public static void PutQn(string qnKey, Qn qn, PostQnCallback callback)
+//    {
+//        RestClient.Put("https://engeenur-17baa.firebaseio.com/qns/" + qnKey + ".json", qn);
+//        //RestClient.Put($"{databaseURL}qns/"+qnKey+".json", qn);//.Then(response => { callback(); });
+//    }
+//}
---
+++ Assets/Scripts/TrQnViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+//using Proyecto26;
+
+public class TrQnViewModel : MonoBehaviour
+{
+    public static string qns;
+    public static string ans;
+    public static int level;
+
+    public string userName = "Alice";
+    public InputField qnsCreateInput;
+    public InputField ansCreateInput;
+    public InputField qnsEditInput;
+    public InputField ansEditInput;
+
+    public GameObject itemParent, item, formCreate, formUpdate;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    read();
+    //}
+
+    //public void read()
+    //{
+    //    for (int i = 0; i < itemParent.transform.childCount; i++)
+    //    {
+    //        Destroy(itemParent.transform.GetChild(i).gameObject);
+    //    }
+    //    DatabaseQAHandler.GetQns(qns =>
+    //    {
+    //        int number = 0;
+    //        foreach (var qn in qns)
+    //        {
+    //            Debug.Log($"{qn.Key} {qn.Value.qns} {qn.Value.ans} {qn.Value.level} {qn.Value.userName}");
+    //            if (qn.Value.userName == userName)
+    //            {
+    //                GameObject tmp_item = Instantiate(item, itemParent.transform);
+    //                tmp_item.name = qn.Key;
+    //                tmp_item.transform.GetChild(0).GetComponent<Text>().text = number.ToString();
+    //                tmp_item.transform.GetChild(1).GetComponent<Text>().text = qn.Value.qns;
+    //                tmp_item.transform.GetChild(2).GetComponent<Text>().text = qn.Value.ans;
+    //                number++;
+    //            }
+    //        }
+    //    });
+    //}
+
+    //public void create()
+    //{
+    //    var qn = new Qn(qnsCreateInput.text, ansCreateInput.text, 1, userName);
+    //    DatabaseQAHandler.PostQn(qn, () => { });
+    //    formCreate.transform.GetChild(1).GetComponent<InputField>().text = "";
+    //    formCreate.transform.GetChild(2).GetComponent<InputField>().text = "";
+    //    read();
+    //}
+
+    //public void delete(GameObject item)
+    //{
+    //    string qnKey = item.name;
+    //    DatabaseQAHandler.DeleteQn(qnKey, () => { });
+    //    read();
+    //}
+
+    //public string qnKey;
+    //public void openFormEdit(GameObject obj_edit)
+    //{
+    //    qnKey = obj_edit.name;
+    //    formUpdate.SetActive(true);
+    //}
+
+    //public void update()
+    //{
+    //    Debug.Log("Qns: " + qnKey);
+    //    DatabaseQAHandler.GetQn(qnKey, qn =>
+    //    {
+    //        Debug.Log($"{qn.qns} {qn.ans} {qn.level} {qn.userName}");
+    //    });
+    //    var question = new Qn(qnsEditInput.text, ansEditInput.text, 1, userName);
+    //    RestClient.Put("https://engeenur-17baa.firebaseio.com/qns/" + qnKey + ".json", question);
+    //    read();
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //    //DatabaseQAHandler.PutQn(qnKey, qn,  () => { });
+    //    //waiter();
+    //    //IEnumerator waiter(){ yield return new WaitForSeconds(10);}
+    //}
+    //public void UpdateQ()
+    //{
+    //    var qn = new Qn(qnsEditInput.text, ansEditInput.text, 1, userName);
+    //    DatabaseQAHandler.PostQn(qn, () => { });
+    //    formUpdate.transform.GetChild(1).GetComponent<InputField>().text = "";
+    //    formUpdate.transform.GetChild(2).GetComponent<InputField>().text = "";
+    //    read();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
