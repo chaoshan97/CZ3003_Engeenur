f749514fa9daa82038028257f8ea23bcb1ef043d
ece2727 <69996862+ece2727@users.noreply.github.com>
2020-10-13 20:45:43 +0800

    updatefl
    updatefl

---
+++ Assets/Scripts/FriendListControllerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendListControllerScript : MonoBehaviour
+{
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+    public void StoreName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = theName;
+    }
+  
+    public void populateFriendList()
+    {
+
+    }
+}
