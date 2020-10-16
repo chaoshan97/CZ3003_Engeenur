411e0454f740455f003b564e93352eb04eeee954
tanbp0009 <tanbp0009@hotmail.com>
2020-10-17 01:58:35 +0800

    Added unity weld Binded monster health to ui text in battleCanvas

---
+++ Assets/Scripts/BattleControllerScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.ComponentModel;
+using UnityWeld.Binding;
+
+[Binding]
+public class BattleControllerScript : MonoBehaviour, INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    public PlayerScript player;
+    public MonsterControllerScript monsterController;
+    public double timer;
+    public GameObject monsterPrefab;
+    public GameObject monsterPos;
+    private GameObject monster;
+    public MonsterScript monsterScript;
+    private float timer2 = 0;
+    private int count = 100;
+
+    [Binding]
+    public int MonsterHealth
+    {
+        get
+        {
+            if (monsterScript)
+                return monsterScript.Health;
+            return 0;
+        }
+        set
+        {
+            if (monsterScript.Health == value)
+            {
+                return; // No change.
+            }
+
+            monsterScript.Health = value;
+
+            if (PropertyChanged != null && monsterScript)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("MonsterHealth"));
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        //monster = new GameObject();
+        //monster = monsterController.getMonster(1);
+        monster = Instantiate(monsterPrefab);
+        monster.transform.SetParent(monsterPos.transform, false);
+        monsterScript = monster.GetComponent<MonsterScript>();
+        monsterScript.init(1, 50, 10);
+    }
+
+    public void SetMonsterHealth(int _health)
+    {
+        monsterScript.Health = _health;
+        //monsterHealth = _health.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //monsterScript = monster.GetComponent<MonsterScript>();
+        // if (monster)
+        //     monster.transform.parent = monsterPos.transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer2 += Time.deltaTime;
+
+        if (timer2 >= 1f)
+        {
+            MonsterHealth = count--;
+            timer2 = 0;
+        }
+    }
+}
--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -7,10 +7,12 @@ public class MonsterScript : MonoBehaviour
     private int id;
     private int health;
     private int damage;
+    private string monsterName;
 
-    public MonsterScript(int _id, int _health, int _damage)
+    public MonsterScript(int _id, string _monsterName, int _health, int _damage)
     {
         id = _id;
+        monsterName = _monsterName;
         health = _health;
         damage = _damage;
     }
@@ -18,6 +20,7 @@ public class MonsterScript : MonoBehaviour
     public int Id { get => id; set => id = value; }
     public int Health { get => health; set => health = value; }
     public int Damage { get => damage; set => damage = value; }
+    public string MonsterName { get => monsterName; set => monsterName = value; }
 
     public void init(int _id, int _health, int _damage)
     {
