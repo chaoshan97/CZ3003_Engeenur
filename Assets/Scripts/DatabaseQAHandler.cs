82e88e9c24db33e8db703f533f421149e858460d
peiyan-lim <py.peiyan@gmail.com>
2020-10-20 02:19:36 +0800

    Deleted unused old files and clean code

--- Assets/Scripts/DatabaseQAHandler.cs
+++ Assets/Scripts/DatabaseQAHandler.cs
@@ -8,10 +8,6 @@ public static class DatabaseQAHandler
 
     private static fsSerializer serializer = new fsSerializer();
 
-    public delegate void DelQnCallback();
-    public delegate void PostQnCallback();
-    public delegate void GetQnCallback(Qn qn);
-    public delegate void GetQnsCallback(Dictionary<string, Qn> qns);
     public delegate void DelCourseCallback();
     public delegate void PostCourseCallback();
     public delegate void GetCourseCallback(Course course);
@@ -21,40 +17,6 @@ public static class DatabaseQAHandler
     public delegate void GetLvlCallback(CourseLvlQn courseLvlQn);
     public delegate void GetLvlsCallback(Dictionary<string, CourseLvlQn> courseLvlQns);
 
-    public static void PostQn(Qn qn, PostQnCallback callback)
-    {
-        RestClient.Post<Qn>($"{databaseURL}qns/.json", qn).Then(response => { callback(); });
-    }
-
-    public static void GetQn(string qnId, GetQnCallback callback)
-    {
-        RestClient.Get<Qn>($"{databaseURL}qns/{qnId}.json").Then(qn => { callback(qn); });
-    }
-
-    public static void GetQns(GetQnsCallback callback)
-    {
-        RestClient.Get($"{databaseURL}qns.json").Then(response =>
-        {
-            var responseJson = response.Text;
-            var data = fsJsonParser.Parse(responseJson);
-            object deserialized = null;
-            serializer.TryDeserialize(data, typeof(Dictionary<string, Qn>), ref deserialized);
-
-            var qns = deserialized as Dictionary<string, Qn>;
-            callback(qns);
-        });
-    }
-
-    public static void DeleteQn(string qnKey, DelQnCallback callback)
-    {
-        RestClient.Delete($"{databaseURL}qns/" + qnKey + ".json").Then(response => { callback(); });
-    }
-
-    public static void PutQn(string qnKey, Qn qn, PostQnCallback callback)
-    {
-        RestClient.Put("https://engeenur-17baa.firebaseio.com/qns/" + qnKey + ".json", qn);
-        //RestClient.Put($"{databaseURL}qns/"+qnKey+".json", qn);//.Then(response => { callback(); });
-    }
 
     public static void PostCourse(Course course, string courseName, PostCourseCallback callback)
     {
